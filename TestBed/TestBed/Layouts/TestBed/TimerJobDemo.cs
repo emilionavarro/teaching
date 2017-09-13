3d831e11962ca19a35d0995d31f4d7f64d2b60ee
SP Farm <spfarm@RecordLion.com>
2017-09-13 11:31:50 -0700

    Added an example timer job

---
+++ TestBed/TestBed/Layouts/TestBed/TimerJobDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+namespace TestBed.Layouts
+{
+    class TimerJobDemo:SPJobDefinition
+    {
+        public TimerJobDemo():base()
+        {
+
+        } 
+
+        public TimerJobDemo (string jobName, SPService service) : base(jobName, service, null, SPJobLockType.None)
+        {
+            this.Title = "Demo Timer Job";
+        }
+
+        public TimerJobDemo(string jobName, SPWebApplication webapp) : base(jobName, webapp, null, SPJobLockType.ContentDatabase)
+        {
+            this.Title = "Demo Timer Job";
+        }
+
+        public override void Execute(Guid targetInstanceId)
+        {
+            base.Execute(targetInstanceId);
+
+            //do stuff
+        }
+    }
+}
